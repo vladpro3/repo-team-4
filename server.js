51a4dfca343cdc6a0c4980810e479c969df0dc24
vladpro3 <Bondarovich77@gmail.com>
2018-05-06 00:23:54 +0300

    deploy

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const {createServer} = require("./src/server/server");
 const {
     MONGO_USER = "repoteam4",
     MONGO_PASSWORD = "repoteam4",
-    MONGO_LOCAL = "false",
     MONGO_DATABSE = "repoteam4",
     MONGO_HOST = "ds157538.mlab.com",
     MONGO_PORT = 57538,
@@ -19,7 +18,7 @@ const DATABASE_CONFIG = {
     password: MONGO_PASSWORD,
     host: MONGO_HOST,
     port: MONGO_PORT,
-    local: MONGO_LOCAL !== "false",
+    local: "false",
     database: MONGO_DATABSE
 };
 
@@ -27,8 +26,10 @@ const DATABASE_CONFIG = {
  * Socket.io server
  */
 const SERVER_CONFIG = {
-    host: "localhost",
-    port: 3001
+    // host: "localhost",
+    // port: 3001
+    host: process.env.HOST || process.env.SERVER_HOST || "localhost",
+    port: process.env.PORT || 8080,
 };
 
 createServer(SERVER_CONFIG, DATABASE_CONFIG)
